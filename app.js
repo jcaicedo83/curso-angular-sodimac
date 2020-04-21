2ec0400642e6866ab27227068b2a1931c1a5e898
Javier Andres Caicedo Lopez <jcaicedo@homecenter.co>
2020-04-21 18:03:39 -0500

    se hace commiy

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+(function () {
+    function sumar(a, b) {
+        console.log(a + b, "sumar");
+    }
+    sumar(7, 7);
+})();
---
+++ app.ts
@@ -0,0 +1,7 @@
+(function (){
+ function sumar (a:number, b:number){
+     console.log(a+b,"sumar");
+ }
+
+ sumar (7,7);
+})();
