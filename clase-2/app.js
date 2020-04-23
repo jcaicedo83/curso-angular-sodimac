0ac8b2d845245598efb8bfd5de9a9d65fa649395
Javier Andres Caicedo Lopez <jcaicedo@homecenter.co>
2020-04-22 19:16:46 -0500

    clase(2)-20200422

---
+++ clase-2/app.js
@@ -0,0 +1,26 @@
+"use strict";
+(() => {
+    /*
+    //funciones anidadas
+    let sumar= (a: number, b: number):number => {
+        let cuadrado=(a:number)=> a*a;
+        return cuadrado(a)+ cuadrado(b);
+    }
+    
+    console.log (sumar(4,3));*/
+    /**PROMESAS********* */
+    const picking = (cant) => {
+        let inventario = 50;
+        return new Promise((success, reject) => {
+            if (cant <= inventario) {
+                success(`Picked ${cant} units!!`);
+            }
+            else {
+                reject("not fulfillable");
+            }
+        });
+    };
+    picking(70)
+        .then(x => console.log(x))
+        .catch(x => console.warn(x));
+})();
